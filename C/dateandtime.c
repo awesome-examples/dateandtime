35a2517e1c7042f8b4c7bd7508b9334331ee7563
Adalberto Caldeira Brant Filho <adalbertobrant@gmail.com>
2018-10-27 16:42:11 -0300

    Create dateandtime.c
    Simple program to show date and time.

---
+++ C/dateandtime.c
@@ -0,0 +1,9 @@
+#include <time.h>
+int main(void){
+
+time_t t = time(NULL);
+struct tm tm = *localtime(&t);
+
+printf("now: %d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+return 0;
+}
